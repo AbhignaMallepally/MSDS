6a41a6f01488c6c925680530304f145448404735
Your Name <abhigna.mallepally@gmail.com>
2025-03-20 20:50:46 +0530

    added frontend

--- MSDS498-Capstone/songfox/app/backend/src/app/services/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Services() {
-    return (
-      <section>
-        <h1 className="text-3xl font-bold">Our Services</h1>
-        <p>We offer a range of services to help your business grow.</p>
-      </section>
-    );
-  }
-  
